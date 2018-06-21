0234912949ea41eb39bcf670e614d0fd3f4e3382
Nuno M <greven.pt@gmail.com>
2018-06-21 13:34:49 +0100

    ⚡️ Update webpack publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
+var path = require('path')
+
 module.exports = {
   configureWebpack: {
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      publicPath: '/cv_playground/'
+    },
     devtool: 'sourcemap'
   }
 }
